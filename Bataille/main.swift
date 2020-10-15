629f778fa6a371146a6bbed0fe67cc8be7cd7cb2
GuillaumeSavin <guisav1996@hotmail.fr>
2020-10-15 10:08:23 +0200

    Initial Commit

---
+++ Bataille/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Bataille
+//
+//  Created by sg812131 on 15/10/2020.
+//  Copyright © 2020 sg812131_savin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
